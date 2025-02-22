9b8b524fb3dd60fcf99fc66dc142dd6a5e50aaf4
aquamarine5 <3168287806@QQ.com>
2025-02-22 23:39:01 +0800

    closed #3 ,fix bug

--- Stackbricks/src/main/java/org/aquamarine5/brainspark/stackbricks/StackbricksPackageFile.kt
+++ Stackbricks/src/main/java/org/aquamarine5/brainspark/stackbricks/StackbricksPackageFile.kt
@@ -2,6 +2,7 @@ package org.aquamarine5.brainspark.stackbricks
 
 import android.content.Context
 import android.content.Intent
+import android.util.Log
 import androidx.core.content.FileProvider
 import java.io.File
 
@@ -9,16 +10,17 @@ open class StackbricksPackageFile(
     private val file: File
 ) {
     open fun installPackage(context: Context) {
-        Intent(Intent.ACTION_VIEW).apply {
+        context.startActivity(Intent(Intent.ACTION_VIEW).apply {
             addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
             addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
+            val uri=FileProvider.getUriForFile(context,
+                "org.aquamarine5.brainspark.stackbricks.file_provider", file)
+            Log.i("StackbricksPackageFile","uri: $uri")
             setDataAndTypeAndNormalize(
-                FileProvider.getUriForFile(context,
-                    "org.aquamarine5.brainspark.stackbricks.file_provider", file),
+                uri,
                 "application/vnd.android.package-archive"
             )
-            context.startActivity(this)
-        }
+        })
     }
 
     open fun clean(){
